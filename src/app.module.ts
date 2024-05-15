73de03d40df970a3468938726354fb9bf5bb8157
김현중 <ethanklocked@EthanKlocked.local>
2024-05-15 14:24:14 +0900

    test

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { EmailModule } from './email/email.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [UserModule, EmailModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
