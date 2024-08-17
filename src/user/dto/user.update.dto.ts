c8bf0c168a6460cfec41be9c4c52968a775e3890
Yoonjae Choi <dbswo9795@gmail.com>
2024-08-17 15:22:16 +0900

    chore: 프리티어 적용

--- src/user/dto/user.update.dto.ts
+++ src/user/dto/user.update.dto.ts
@@ -1,32 +1,32 @@
 import { IsString, IsBoolean, IsOptional } from 'class-validator';
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UserUpdateDto {
-    @IsString()
-    @IsOptional()
-    @ApiProperty({ 
-        description: 'name', 
-        example: 'ethank',
-        required: false
-    })  
-    name?: string;     
+  @IsString()
+  @IsOptional()
+  @ApiProperty({
+    description: 'name',
+    example: 'ethank',
+    required: false,
+  })
+  name?: string;
 
-    @IsBoolean()
-    @IsOptional()
-    @ApiProperty({ 
-        description: 'inApp alarm', 
-        example: true, 
-        default: true,
-        required: false
-    })
-    alarm?: boolean;
+  @IsBoolean()
+  @IsOptional()
+  @ApiProperty({
+    description: 'inApp alarm',
+    example: true,
+    default: true,
+    required: false,
+  })
+  alarm?: boolean;
 
-    @IsString()
-    @IsOptional()
-    @ApiProperty({ 
-        description: 'profile image', 
-        example: 'https://example.com/profile.jpg',
-        required: false
-    })
-    imgUrl?: string;
+  @IsString()
+  @IsOptional()
+  @ApiProperty({
+    description: 'profile image',
+    example: 'https://example.com/profile.jpg',
+    required: false,
+  })
+  imgUrl?: string;
 }
