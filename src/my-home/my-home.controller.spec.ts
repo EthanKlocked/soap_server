1f0beb918da7c5f159e95adfcdc944f08c37f1bc
Yoonjae Choi <dbswo9795@gmail.com>
2024-09-10 22:10:50 +0900

    feat: my home controller 추가

---
+++ src/my-home/my-home.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { MyHomeController } from './my-home.controller';
+
+describe('MyHomeController', () => {
+  let controller: MyHomeController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [MyHomeController],
+    }).compile();
+
+    controller = module.get<MyHomeController>(MyHomeController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
