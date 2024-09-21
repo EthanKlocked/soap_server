c5dc2e649f3e1e589044033ff102ccd51ee16507
김현중 <ethanklocked@EthanKlocked.local>
2024-09-21 11:11:19 +0900

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,15 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
+	app.enableCors({
+		origin: '*',
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		preflightContinue: false,
+		optionsSuccessStatus: 204,
+		credentials: true,
+		allowedHeaders: 'Content-Type, Accept, Authorization'
+	});
+
 	app.useGlobalPipes(
 		new ValidationPipe({
 			whitelist: true,
