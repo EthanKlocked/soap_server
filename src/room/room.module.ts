babb0ec94c2038edc5bf1237af64709cda3f3dac
Sam Choi <dbswo9795@gmail.com>
2024-10-27 22:14:13 +0900

    fix: 유저 정보와 친구 상태 추가

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RoomController } from './room.controller';
 import { RoomService } from './room.service';
+import { UserModule } from '@src/user/user.module';
+import { FriendModule } from '@src/friend/friend.module';
 import { Room, RoomSchema } from './schema/room.schema';
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: Room.name, schema: RoomSchema }])],
+	imports: [
+		MongooseModule.forFeature([{ name: Room.name, schema: RoomSchema }]),
+		UserModule,
+		FriendModule
+	],
 	controllers: [RoomController],
 	providers: [RoomService],
 	exports: [RoomService]
