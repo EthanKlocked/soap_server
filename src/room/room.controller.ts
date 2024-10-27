58050e6057df3b5b1be5a23777774a98aee420d2
Sam Choi <dbswo9795@gmail.com>
2024-10-27 20:33:53 +0900

    fix: post 요청 삭제 및, 수정할 떄 기본값 추가해서 수정하도록 변경

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,7 +1,6 @@
 import {
 	Body,
 	Controller,
-	Post,
 	UseGuards,
 	Get,
 	Request,
@@ -25,7 +24,6 @@ import {
 import { JwtAuthGuard } from '@src/auth/guard/jwt.guard';
 import { ApiGuard } from '@src/auth/guard/api.guard';
 import { RoomService } from './room.service';
-import { CreateRoomDto } from './dto/room.create.dto';
 import { UpdateRoomDto } from './dto/room.update.dto';
 import { ItemDto } from './dto/room-items.dto';
 import { UpdateItemDto } from './dto/update-items.dto';
@@ -71,22 +69,6 @@ export class RoomController {
 		}
 	}
 
-	@Post()
-	@ApiOperation({
-		summary: '방 꾸미기 구현',
-		description: '내 방 꾸미기. 현재 로그인한 사용자의 ID가 자동으로 할당됩니다.'
-	})
-	@ApiBody({
-		type: CreateRoomDto
-	})
-	@ApiResponse({ status: 201, description: 'Success' })
-	@ApiResponse({ status: 400, description: 'Request without API KEY' })
-	@ApiResponse({ status: 403, description: 'Invalid API KEY' })
-	@ApiResponse({ status: 500, description: 'Server Error' })
-	async createOrUpdateRoom(@Request() req, @Body() createRoomDto: Omit<CreateRoomDto, 'userId'>) {
-		return this.roomService.create(req.user.id, createRoomDto);
-	}
-
 	@Patch()
 	@ApiOperation({
 		summary: '내 방 데이터 수정',
@@ -112,7 +94,6 @@ export class RoomController {
 	@ApiResponse({ status: 200, description: 'Success' })
 	@ApiResponse({ status: 400, description: 'Request without API KEY' })
 	@ApiResponse({ status: 403, description: 'Invalid API KEY' })
-	@ApiResponse({ status: 404, description: 'Room not found' })
 	@ApiResponse({ status: 500, description: 'Server Error' })
 	async updateRoom(@Request() req, @Body() updateRoomDto: UpdateRoomDto | UpdateItemDto) {
 		return this.roomService.update(req.user.id, updateRoomDto);
