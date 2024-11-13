e874c2a0a381762e577a3ee50fbdbde81a7af2a6
EthanStoa <ethanklocked@stoa-investment.com>
2024-11-13 14:27:01 +0900

    compose modified

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -9,7 +9,7 @@ export const dbConfig = {
 		const db_user = configService.get<string>('DB_USER_CUSTOM');
 		const db_password = configService.get<string>('DB_PASSWORD_CUSTOM');
 		return {
-			uri: `mongodb://${db_user}:${encodeURIComponent(db_password)}@${db_path}`,
+			uri: `mongodb://${db_user}:${encodeURIComponent(db_password)}@${db_path}?authSource=admin`,
 			dbName: db_name
 		};
 	}
